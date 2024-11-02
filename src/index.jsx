4d96064c562126ce1f7bd1b0b8416eb1fbdd108c
Himanshu Kumar <i.himu.kumar23@gmail.com>
2024-11-02 23:30:34 +0530

    Adding basic compare UI page (#7)

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const { useEffect, useState } = React;
-
-function App() {
-	const [message, setMessage] = useState('');
-
-	useEffect(() => {
-		fetch('/api/message')
-			.then((response) => response.json())
-			.then((data) => setMessage(data.message))
-			.catch((error) => console.error('Error fetching message:', error));
-	}, []);
-
-	return (
-		<div>
-			<h1>My App</h1>
-			<p>{message}</p>
-		</div>
-	);
-}
+import App from './App';
 
 // Render the React app
 const root = ReactDOM.createRoot(document.getElementById('root'));
