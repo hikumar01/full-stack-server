6961d3adb3f83d2389df293a7b56f7f9981d208f
Himanshu Kumar <i.himu.kumar23@gmail.com>
2024-11-03 03:31:38 +0530

    Integrating eslint (#9)

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,59 @@ import InputDiv from './component/InputDiv.jsx';
 import React from 'react';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.inputDiv1Ref = React.createRef();
-		this.inputDiv2Ref = React.createRef();
-		this.state = {
-			isDivEditable: true
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.inputDiv1Ref = React.createRef();
+        this.inputDiv2Ref = React.createRef();
+        this.state = {
+            isDivEditable: true
+        };
+    }
 
-	handleCompareClick = () => {
-		if (this.state.isDivEditable) {
-			const input1Value = this.inputDiv1Ref.current.getValue();
-			const input2Value = this.inputDiv2Ref.current.getValue();
-			console.log('Input 1:', input1Value);
-			console.log('Input 2:', input2Value);
-		}
-		this.setState({ isDivEditable: !this.state.isDivEditable });
-	}
+    handleCompareClick = () => {
+        if (this.state.isDivEditable) {
+            const input1Value = this.inputDiv1Ref.current.getValue();
+            const input2Value = this.inputDiv2Ref.current.getValue();
+            console.log('Input 1:', input1Value);
+            console.log('Input 2:', input2Value);
+        }
+        this.setState({isDivEditable: !this.state.isDivEditable});
+    };
 
-	getButtonText() {
-		return this.state.isDivEditable ? 'Compare' : 'Reset';
-	}
+    getButtonText() {
+        return this.state.isDivEditable ? 'Compare' : 'Reset';
+    }
 
-	handleDivClick = (inputValue) => {
-		console.log('Div clicked:', inputValue);
-		this.setState({ isDivEditable: !this.state.isDivEditable });
-	}
+    handleDivClick = (inputValue) => {
+        console.log('Div clicked:', inputValue);
+        this.setState({isDivEditable: !this.state.isDivEditable});
+    };
 
-	render() {
-		return (
-			<div className='appContainer'>
-				<div className='button-container'>
-					<button
-						className='compareButton'
-						onClick={this.handleCompareClick} >
-						{this.getButtonText()}
-					</button>
-				</div>
-				<div className='vertical-container'>
-					<InputDiv
-						ref={this.inputDiv1Ref}
-						inputString="Enter String 1"
-						isDivEditable={this.state.isDivEditable}
-						onDivClick={this.handleDivClick} />
-					<InputDiv
-						ref={this.inputDiv2Ref}
-						inputString="Enter String 2"
-						isDivEditable={this.state.isDivEditable}
-						onDivClick={this.handleDivClick} />
-				</div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className='appContainer'>
+                <div className='button-container'>
+                    <button
+                        className='compareButton'
+                        onClick={this.handleCompareClick} >
+                        {this.getButtonText()}
+                    </button>
+                </div>
+                <div className='vertical-container'>
+                    <InputDiv
+                        ref={this.inputDiv1Ref}
+                        inputString='Enter String 1'
+                        isDivEditable={this.state.isDivEditable}
+                        onDivClick={this.handleDivClick} />
+                    <InputDiv
+                        ref={this.inputDiv2Ref}
+                        inputString='Enter String 2'
+                        isDivEditable={this.state.isDivEditable}
+                        onDivClick={this.handleDivClick} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
